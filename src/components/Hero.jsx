eb91ab0a50145f2cd95c1263c05aa18e6d9f6ed6
Charlie Emanuel Daga-as <charlieemanuel8@gmail.com>
2025-11-04 16:19:18 +0800

    continuing hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,27 +1,59 @@
-import React from 'react'
-import carouselOne from '../assets/images/carousel-1.jpg'
+import React from 'react';
+import carouselOne from '../assets/images/carousel-1.jpg';
+import heroBanner from '../assets/images/home-banner-2.jpg';
 
 const Hero = () => {
   return (
-      <div className='flex flex-col md:flex-row items-center md:justify-between max-md:gap-6'>
-        <div className='flex flex-col items-center md:items-start justify-between h-full gap-5'>
-          <h2 className='text-2xl md:text-3xl'>Power Up Your World with the Latest Devices</h2>
-          <p className='text-base'>
-            Discover next-gen smartphones, computers, and smart home gadgets - all designed to keep you connected and in control.
-          </p>
-          <div className='flex flex-row items-center justify-between gap-3 md:gap-8'>
-            <button className='bg-[#EDE8D0] px-10 py-1 text-lg font-semibold rounded-md cursor-pointer hover:bg-black hover:text-[#EDE8D0] transition-all duration-300'>
-              Shop Now
-            </button>
-            <button className='bg-[#EDE8D0] px-10 py-1 text-lg font-semibold rounded-md cursor-pointer hover:bg-black hover:text-[#EDE8D0] transition-all duration-300'>
-              New Arrival
-            </button>
+      <section>
+
+        {/* Text, CTA, Carousel */}
+        <div className='flex flex-col md:flex-row items-center md:justify-between max-md:gap-6 md:space-x-2'>
+          <div className='flex flex-col items-center md:items-start justify-between h-full gap-5'>
+            <h2 className='text-2xl md:text-3xl'>Power Up Your World with the Latest Devices</h2>
+            <p className='text-base'>
+              Discover next-gen smartphones, computers, and smart home gadgets - all designed to keep you connected and in control.
+            </p>
+            <div className='flex flex-row items-center justify-between gap-3 md:gap-8'>
+              <button className='bg-[#EDE8D0] px-10 py-1 text-lg font-semibold rounded-md cursor-pointer hover:bg-black hover:text-[#EDE8D0] transition-all duration-300'>
+                Shop Now
+              </button>
+              <button className='bg-[#EDE8D0] px-10 py-1 text-lg font-semibold rounded-md cursor-pointer hover:bg-black hover:text-[#EDE8D0] transition-all duration-300'>
+                New Arrival
+              </button>
+            </div>
+          </div>
+
+          {/* Carousel Images */}
+          <div className='h-full'>
+            <img src={carouselOne} alt='image 1' width={380} className='rounded-lg'></img>
           </div>
         </div>
-        <div className='h-full'>
-          <img src={carouselOne} alt='image 1' width={380} className='rounded-lg'></img>
+
+        {/* Banner */}
+        <div className='relative w-full max-md:max-h[200px] md:h-[200px] rounded-lg my-5 banner overflow-hidden'>
+          <img src={heroBanner} alt='banner' className='object-bottom w-full' ></img>
+          <div className='absolute bottom-3 right-3 space-y-3 text-right'>
+            <h2 className='max-md:hidden text-3xl text-white text-shadow-lg text-shadow-black'>Big Tech. Bigger Savings.</h2>
+            <h3 className='text-2xl text-white text-shadow-lg text-shadow-black'>
+              Get up to 35% off on selected devices this week only.
+            </h3>
+          </div>
         </div>
-      </div>
+
+        {/* Cards */}
+        <div className='flex flex-col items-center justify-center'>
+
+          {/* Cards Title */}
+          <div>
+            <h3 className='text-xl'>Explore By Categories</h3>
+          </div>
+
+          {/* Cards Item */}
+          <div></div>
+        </div>
+      </section>
+
+
   )
 }
 export default Hero
